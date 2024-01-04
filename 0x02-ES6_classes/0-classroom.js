ae736e8f5b4045abe9cd19736242480d787ee39f
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-01-04 14:18:12 -0800

    0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,18 +1,13 @@
 /**
- * Class representing a classroom.
+ * Represents a class room.
  */
 export default class ClassRoom {
   /**
-   * Creates a new instance of the ClassRoom class.
-   * @param {number} maxStudentsSize - The maximum number of students in the class.
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
    */
   constructor(maxStudentsSize) {
-    /**
-     * The maximum number of students in the class.
-     * @type {number}
-     * @private
-     */
     this._maxStudentsSize = maxStudentsSize;
   }
 }
-
