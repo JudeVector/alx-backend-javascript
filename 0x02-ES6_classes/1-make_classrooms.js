b270ad919c7708c7503801aad2171c841c2ed9ca
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-01-04 14:18:12 -0800

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,9 @@
 import ClassRoom from './0-classroom';
 
 /**
- * Initializes an array of {@link ClassRoom}s with specified sizes.
- * 
- * @returns {ClassRoom[]} An array of {@link ClassRoom}s.
+ * Creates an array of {@link ClassRoom}s with a specific size.
+ * @returns An array of {@link ClassRoom}s.
  */
 export default function initializeRooms() {
-  return [19, 20, 34].map(size => new ClassRoom(size));
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
-
